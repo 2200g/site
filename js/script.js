7d479bf4c4dc9b1f304d4ef4b0eb6447e06d9623
aadi <aadi@aadi.net.in>
2023-06-20 14:31:54 +0530

    1021

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function randomNumber(min, max) {
 
 function getRandomImage() {
 	    path = '/pics/'
-	    var num = randomNumber(1000, 1020);;
+	    var num = randomNumber(1000, 1021);;
 	    var img = path + num + '.jpg';
 	    return img;
 }
