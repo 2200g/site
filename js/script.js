700a3cda9c5cf7ddacb9a420e4006220f5e176fe
2200g <aadi@aadi.net.in>
2026-03-12 09:06:26 +0530

    refactor

--- js/script.js
+++ js/script.js
@@ -1,22 +1,32 @@
-function randomNumber(min, max) { 
-	    return Math.floor(Math.random() * (max - min) + min);
-} 
+(function () {
+  const IMAGE_DIR = '/pics/';
+  const IMAGE_MIN = 1000;
+  const IMAGE_MAX = 1022;
+  const GALLERY_COUNT = 3;
 
-function getRandomImage() {
-	    path = '/pics/'
-	    var num = randomNumber(1000, 1022);;
-	    var img = path + num + '.jpg';
-	    return img;
-}
+  function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-	    var gal = document.getElementById('img-gallery');
-	    for(var i = 0; i < 3; i++){
-		    	var img = document.createElement("img");
-		    	img.src = getRandomImage();
-		    	img.alt = "random aesthetic pic";
-          img.width = "237";
-          img.height = "237";
-		    	gal.appendChild(img);
-		        }
-});
+  function getRandomImageSrc() {
+    return IMAGE_DIR + randomInt(IMAGE_MIN, IMAGE_MAX) + '.jpg';
+  }
+
+  function buildGallery(container) {
+    for (var i = 0; i < GALLERY_COUNT; i++) {
+      var img = document.createElement('img');
+      img.src = getRandomImageSrc();
+      img.alt = 'random aesthetic pic';
+      img.width = 237;
+      img.height = 237;
+      container.appendChild(img);
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', function () {
+    var gallery = document.getElementById('img-gallery');
+    if (gallery) {
+      buildGallery(gallery);
+    }
+  });
+})();
